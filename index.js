c13ce779394e2b160e273c9c86cfb8437fffb8cc
sy8984387-cmd <sy8984387@gmail.com>
2026-06-26 12:59:40 +0530

    main

--- index.js
+++ index.js
@@ -31,6 +31,41 @@ app.post("/students",(req,res)=>{
     });
 });
 
+//Data Update
+app.put("/student/:id",(req,res)=>{
+    //constant id = req.params.id;
+    const { id } =req.params;
+    const student = students.find(s => s.id == id);
+    //if student found = value...
+    if(!student)
+    {
+        return res.status(404).json({
+            message:"Student Not Found"
+        });
+    }
+    student.name = req.body.name;
+    student.city = req.body.city;
+
+    res.json({
+        message:"Record Updated",
+        student
+    });
+});
+
+app.delete("/student/:id",(req,res)=>{
+    const id = res.params.id;
+    const student = student.find (s => s.id ==id);
+    if (!student){
+        return res.student(404).json({message:"Invalid ID"});
+    }
+    student = student.filter(s=> s.id !=id);
+    res.json({
+        message:"Record Deleted",
+        students
+    });
+});
+
+
 app.listen(3004,()=>{
     console.log("Server Started");
 });
